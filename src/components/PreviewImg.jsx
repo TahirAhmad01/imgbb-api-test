dcb5f445e3741cdc2abb15189395530eed6fc79b
Tahir ahmad <tahirahmad0234@gmail.com>
2023-10-23 18:24:45 +0600

    coppy to clipbord added

--- src/components/PreviewImg.jsx
+++ src/components/PreviewImg.jsx
@@ -1,29 +1,43 @@
+import { useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+
 /* eslint-disable react/prop-types */
 function PreviewImg({ uploadImageData, loading }) {
+  const [copy, setCopy] = useState(false);
   const { data } = uploadImageData || {};
-  const { url } = data || {};
-  console.log(url);
+  const { url, delete_url } = data || {};
+  console.log(copy);
 
   return (
-    <div className="w-full h-full bg-gray-50 overflow-hidden">
+    <div className="w-full h-full overflow-hidden">
       {uploadImageData !== "" ? (
         <div className="flex flex-col gap-5 justify-between h-full">
-          <div className="flex-grow rounded-md overflow-hidden">
+          <div className="flex-grow rounded-md overflow-hidden bg-gray-50">
             <img src={url} className="w-full h-full object-cover" />
           </div>
-          <div className="flex gap-4">
-            <button
-              type="button"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm w-full py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          <div className="grid grid-cols-2 gap-4">
+            <CopyToClipboard text={url} onCopy={() => setCopy(true)}>
+              <button
+                type="button"
+                className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm w-full py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none "
+              >
+                Copy image url
+              </button>
+            </CopyToClipboard>
+
+            <a
+              href={delete_url}
+              target="_blank"
+              className="block"
+              rel="noreferrer"
             >
-              Copy image url
-            </button>
-            <button
-              type="button"
-              className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm w-full py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800"
-            >
-              Delete Image
-            </button>
+              <button
+                type="button"
+                className="text-white bg-red-700 hover:bg-red-800  font-medium rounded-lg text-sm w-full py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 "
+              >
+                Delete Image
+              </button>
+            </a>
           </div>
         </div>
       ) : (
