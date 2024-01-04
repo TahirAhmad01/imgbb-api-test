0d9101527d5c8bf5f3d031adea66b540ae466161
Tahir Ahmad <tahirahmad0234@gmail.com>
2024-01-04 10:23:08 +0600

    fix color theme

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ import Card from "./components/Card";
 
 function App() {
   return (
-    <div className="bg-gray-50 bg-[url('/bgimg.jpg')] bg-repeat relative bg-blur-sm ">
+    <div
+      className="bg-gray-50 bg-[url('/bgimg.jpg')] bg-repeat relative bg-blur-sm light"
+      style={{ colorScheme: "light" }}
+    >
       <div className="absolute h-full w-full backdrop-blur-[2px] bg-black/3 z-10"></div>
       <div className="max-w-screen-lg mx-auto min-h-screen flex items-center justify-center relative z-30 py-8">
         <Card />
